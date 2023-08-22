31adaa498528b56022c6c19673f65dcdf972ba6c
edsouz01 <x9h2q@students.cs.ubc.ca>
2023-08-22 22:49:28 +0200

    sketch pad can now be resized to users liking

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const grid = document.querySelector('.grid');
-
-
-function createDivs() {
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+function createSquares(dimension = 16) {
+    let grid = document.getElementById('grid');
+    grid.style.width = `${dimension * 20}px`;
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
             let newSquare = document.createElement('div');
             newSquare.className = 'square';
             grid.appendChild(newSquare);
@@ -11,11 +10,27 @@ function createDivs() {
     }
 }
 
-createDivs();
-
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
+function enableDrawing() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
     square.addEventListener('mouseover', () => {
         square.classList.add('touched-square');
     });
 });
+}
+
+function changeDimensions() {
+    let grid = document.getElementById('grid');
+    let newDimensions = prompt('How many squares per side do you want:');
+    if (newDimensions <= 100) {
+        grid.innerHTML = '';
+        grid.style.width = `${newDimensions * 20}px`;
+        createSquares(newDimensions);
+        enableDrawing();
+    }
+    
+}
+
+createSquares();
+
+enableDrawing()
